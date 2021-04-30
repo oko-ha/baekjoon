747ae2c45674ec8c766c70f9ecb7cc5199c94fc5
oko-ha <niceyha0@naver.com>
2021-04-30 23:46:21 +0900

    2021.04.30

--- 12015.py
+++ 12015.py
@@ -4,6 +4,7 @@
 # 난이도 : 골드 II
 # 분류 : 이분 탐색, 가장 긴 증가하는 부분 수열: O(n log n)
 
+# binary search 구현
 import sys
 n = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().split()))
@@ -24,4 +25,15 @@ for i in range(1, n):
                 index = mid
                 break
         lis[index] = arr[i]
+print(len(lis))
+
+# bisect 모듈 사용
+from bisect import bisect_left
+lis.clear()
+lis = [arr[0]]
+for i in range(1, n):
+    if lis[-1] < arr[i]:
+        lis.append(arr[i])
+    else:
+        lis[bisect_left(lis, arr[i])] = arr[i]
 print(len(lis))
