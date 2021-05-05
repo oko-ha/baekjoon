481d7ae6f0b3201d01b757d94c8be0a279c0a56f
oko-ha <niceyha0@naver.com>
2021-05-05 19:05:22 +0900

    2021.05.05

--- 1655.py
+++ 1655.py
@@ -2,13 +2,23 @@
 # 이름 : 가운데를 말해요
 # 번호 : 1655
 # 난이도 : 골드 II
-# 분류 : 
+# 분류 : 자료 구조, 우선순위 큐
 
 import sys
 import heapq
-heap = []
-for _ in range(int(sys.stdin.readline().rstrip())):
-    x = int(sys.stdin.readline().rstrip())
-    heapq.heappush(heap, (x, x))
-
-# https://pongdangstory.tistory.com/471
+input = sys.stdin.readline
+n = int(input())
+leftHeap = [-int(input())]
+rightHeap = []
+print(-leftHeap[0])
+for _ in range(n - 1):
+    x = int(input())
+    if x >= -leftHeap[0]:
+        heapq.heappush(rightHeap, x)
+    else:
+        heapq.heappush(leftHeap, -x)
+    if len(rightHeap) > len(leftHeap):
+        heapq.heappush(leftHeap, -heapq.heappop(rightHeap))
+    elif len(leftHeap) > len(rightHeap) + 1:
+        heapq.heappush(rightHeap, -heapq.heappop(leftHeap))
+    print(-leftHeap[0])
