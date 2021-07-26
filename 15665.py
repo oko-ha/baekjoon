bef605aa0392d4b47eac3066629980dd21fe8c69
oko-ha <niceyha0@naver.com>
2021-07-26 15:11:09 +0900

    2021.07.26

--- 15665.py
+++ 15665.py
@@ -1,6 +1,6 @@
-# https://www.acmicpc.net/problem/15664
-# 이름 : N과 M (10)
-# 번호 : 15664
+# https://www.acmicpc.net/problem/15665
+# 이름 : N과 M (11)
+# 번호 : 15665
 # 난이도 : 실버 II
 # 분류 : 백트래킹
 
@@ -9,15 +9,15 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
 ans = []
-def backtracking(k, x):
+def backtracking(k):
     if k < M:
         overlap = 0
-        for i in range(x, N):
-            if  overlap != arr[i]:
+        for i in range(N):
+            if overlap != arr[i]:
                 overlap = arr[i]
                 ans.append(arr[i])
-                backtracking(k + 1, i + 1)
+                backtracking(k + 1)
                 ans.pop()
     else:
         print(*ans)
-backtracking(0, 0)
+backtracking(0)
