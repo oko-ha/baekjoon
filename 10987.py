2c14da6ae572e498d720b6199ec9f8b2c3c34eca
oko-ha <niceyha0@naver.com>
2021-08-12 21:40:03 +0900

    2021.08.12

---
+++ 10987.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/10987
+# 이름 : 모음의 개수
+# 번호 : 10987
+# 난이도 : 브론즈 II
+# 분류 : 
+
+import sys
+import re
+print(len(re.sub('[^a|e|i|o|u]', '', sys.stdin.readline().strip())))
